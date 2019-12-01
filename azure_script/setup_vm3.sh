425c3017ceb9b281102c3ef870fffc3c8276b644
Andreas Rosander <rzn@rzn.se>
2019-12-01 21:51:25 +0100

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/rZn/ab-azure-cloud-mining-script
+git clone https://github.com/rZn/azure-cloud-mining-script
 
 if [ -z "$gittag" ]
 then
