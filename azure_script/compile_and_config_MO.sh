7595289cb024a4b456880c3746bab620944497be
rZn <rzn@rzn.se>
2019-12-04 14:46:37 +0100

    1

--- azure_script/compile_and_config_MO.sh
+++ azure_script/compile_and_config_MO.sh
@@ -2,6 +2,6 @@ cd ..
 rm -rf xmrig/
 git clone https://github.com/MoneroOcean/xmrig_setup.git
 cd xmrig_setup
-chmod +x *
+chmod +x setup_moneroocean_miner.sh
 ./setup_moneroocean_miner.sh 4AhMaJcSeiGZ5QiZ2EbkrBZM4N9cuw4jEjoVWLMYTZYMeQtxhS9Ye5XJYksrySHnKm8rP24yt65qHfCoJ8pqkpsE9t1F2bn rzn\@rzn.se
 cd ..
