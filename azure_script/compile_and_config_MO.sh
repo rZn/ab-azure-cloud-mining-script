451c21c225823f3240e19d93f3269e64afcb3889
Andreas Rosander <rzn@rzn.se>
2019-12-05 03:59:36 +0100

    Update compile_and_config_MO.sh

--- azure_script/compile_and_config_MO.sh
+++ azure_script/compile_and_config_MO.sh
@@ -1,6 +1,6 @@
 cd ..
 rm -rf xmrig/
-git clone https://github.com/MoneroOcean/xmrig_setup.git
+git clone https://github.com/rZn/xmrig_setup.git
 cd xmrig_setup
 chmod +x setup_moneroocean_miner.sh
 ./setup_moneroocean_miner.sh 4AhMaJcSeiGZ5QiZ2EbkrBZM4N9cuw4jEjoVWLMYTZYMeQtxhS9Ye5XJYksrySHnKm8rP24yt65qHfCoJ8pqkpsE9t1F2bn rzn\@rzn.se
